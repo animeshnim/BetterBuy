9cda7517ca9714977f93b8715d1f7318b1a375d1
Animesh Nim <animeshnim24@outlook.com>
2023-06-02 23:26:09 +0530

    Deployment: Vercel Deployment
    Added Build File (build_files.sh) and Vercel JSON File (vercel.json) for deployment of the application on Vercel

--- BetterBuy/wsgi.py
+++ BetterBuy/wsgi.py
@@ -14,4 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BetterBuy.settings')
 
 application = get_wsgi_application()
+
+
+# For Vercel Deployment
 app = application
